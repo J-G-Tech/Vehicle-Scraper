26a265a71502b23d1aa0d465da25825c8dbb5e10
gavishap <71888304+gavishap@users.noreply.github.com>
2023-11-08 01:54:20 +0200

    Inital scraping mechanism finished

--- test.py
+++ test.py
@@ -1,22 +1,89 @@
+import os
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
 
-def download_html(url):
-    response = requests.get(url)
-    with open('webpage.html', 'w') as file:
-        file.write(response.text)
+def scrape_links_and_download_html(driver_path, page_url):
+    driver = webdriver.Chrome(executable_path=driver_path)
+    driver.get(page_url)
+
+    elements = driver.find_elements(By.CLASS_NAME, 'hero-carousel__item--viewvehicle')
+    links = set()
+
+    for element in elements:
+        link = element.get_attribute('href')
+        if link:
+            links.add(link)
+
+    html_contents = []
+
+    if not links:
+        print("No links found with the specified class name.")
+    else:
+        for link in links:
+            # Check if the HTML file for this link already exists in the downloaded_html_files directory
+            file_name = 'downloaded_html_files/' + link.split('/')[-1] + '.html'
+            if os.path.exists(file_name):
+                print(f"HTML file for {link} already exists. Skipping download.")
+                continue
+
+            driver.get(link)
+            html_content = driver.page_source
+
+            if not html_content:
+                print("No HTML content found for the current page.")
+            else:
+                html_contents.append(html_content)
+
+    if not html_contents:
+        print("No HTML contents to write to files.")
+    else:
+        # Create a directory for the downloaded HTML files if it doesn't exist
+        if not os.path.exists('downloaded_html_files'):
+            os.makedirs('downloaded_html_files')
+
+        for i, content in enumerate(html_contents, start=1):
+            with open(f'downloaded_html_files/linked_page_{i}.html', 'w', encoding='utf-8') as file:
+                file.write(content)
+
+    driver.quit()
+    return html_contents
+
+def extract_vehicle_features(html_file):
+    with open(html_file, 'r', encoding='utf-8') as file:
+        html_content = file.read()
+
+    soup = BeautifulSoup(html_content, 'html.parser')
+    vehicle_highlights_list = soup.find('ul', {'id': 'vehicle-highlights-list'})
+
+    vehicle_features = []
+    if vehicle_highlights_list:
+        vehicle_highlights_items = vehicle_highlights_list.find_all('li')
+        for item in vehicle_highlights_items:
+            feature_label = item.find('span')
+            if feature_label:
+                vehicle_features.append(feature_label.text.strip())
+
+    return vehicle_features
+
+def main():
+    #driver_path = 'chromedriver.exe'
+    #page_url = "https://www.pinkertonlynchburg.com/new-trucks.html"
+    #html_contents = scrape_links_and_download_html(driver_path, page_url)
+    # Test the first HTML file
+    #if html_contents:
+        first_html_file = 'downloaded_html_files/linked_page_1.html'
+        features = extract_vehicle_features(first_html_file)
+        print(features)
+
+if __name__ == "__main__":
+    main()
 
 
-#test
-
-
-
-driver = webdriver.Chrome(executable_path='C:/Users/gavis/OneDrive/Desktop/Projects/Vehicl Scraper/Vehicle-Scraper/chromedriver.exe')
-driver.get("https://www.pinkertonlynchburg.com/searchnew.aspx")
-
-carousel_items = driver.find_elements(By.CLASS_NAME, "vehicle-image")
-
-for item in carousel_items:
-    item.click()
-    download_html(driver.current_url)
+#TO-DO:
+#Scrape: MPG, Car title, vehicle information, price
+#then we need to categorize these features into the facebook features
